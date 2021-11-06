983768fef7dcc2ba120f3464f1e17f77b8714d8a
aymene <aymene98@icloud.com>
2021-11-06 14:11:47 +0100

    adding word embeddings

--- src/draft.py
+++ src/draft.py
@@ -1,12 +1,23 @@
 import fasttext.util
 import fasttext
 
-"""fasttext.util.download_model('en', if_exists='ignore') # English
+fasttext.util.download_model('en', if_exists='ignore') # English
 fasttext.util.download_model('fr', if_exists='ignore') # French
 fasttext.util.download_model('pt', if_exists='ignore') # Portuguese
 fasttext.util.download_model('it', if_exists='ignore') # Italian
-"""
+
+ft = fasttext.load_model('cc.en.300.bin')
+fasttext.util.reduce_model(ft, 100)
+ft.save_model('cc.en.100.bin')
+
+ft = fasttext.load_model('cc.fr.300.bin')
+fasttext.util.reduce_model(ft, 100)
+ft.save_model('cc.fr.100.bin')
+
+ft = fasttext.load_model('cc.pt.300.bin')
+fasttext.util.reduce_model(ft, 100)
+ft.save_model('cc.pt.100.bin')
+
 ft = fasttext.load_model('cc.it.300.bin')
 fasttext.util.reduce_model(ft, 100)
-# to get the vector : ft.get_word_vector(word)
 ft.save_model('cc.it.100.bin')
